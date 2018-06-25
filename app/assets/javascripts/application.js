142150083b4bf427c977d15673f46233142a05f2
= <=>
2018-06-25 09:24:50 +0530

    first commit

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,29 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's
+// vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file. JavaScript code in this file should be added after the last require_* statement.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery-ui.min
+//= require bootstrap.min
+//= require jquery-migrate-1.1.0.min
+//= require jquery.slimscroll.min
+//= require jquery.cookie
+//= require jquery.gritter
+//= require jquery.flot.min
+//= require jquery.flot.time.min
+//= require jquery.flot.resize.min
+//= require jquery.flot.pie.min
+//= require jquery.sparkline
+//= require jquery-jvectormap.min
+//= require jquery-jvectormap-world-mill-en
+//= require bootstrap-datepicker
+//= require dashboard.min
+//= require turbolinks
+//= require_tree .
